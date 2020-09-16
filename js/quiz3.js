0c6c67187bd3ef415480cfdd0e8e040180b852b9
tamza1094 <50855674+tamza1094@users.noreply.github.com>
2020-09-16 15:24:51 +0700

    Create quiz3.js

---
+++ js/quiz3.js
@@ -0,0 +1,19 @@
+function check(g){
+    if(g >=81 && g<=100){
+        console.log("A")
+    }
+    else if(g>=71 && g<=80){
+        console.log("B")
+
+    }
+    else if(g>=61 && g<=70){
+        console.log("C")
+    }
+    else if(g>=51 && g<=60){
+        console.log("D")
+    }
+    else if(g>=0 && g<=50){
+        console.log("F")        
+    }    
+}
+check(81)
