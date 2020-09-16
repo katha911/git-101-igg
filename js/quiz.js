f61545327358884590aa85cd92db042f1ec979f2
tamza1094 <50855674+tamza1094@users.noreply.github.com>
2020-09-16 13:52:13 +0700

    Update quiz.js

--- js/quiz.js
+++ js/quiz.js
@@ -1,9 +1,9 @@
 let sir = {
-    name = 'kathawut',
-    lastname= 'mangkalad',
-    age = 15,
-    nickname ='tam',
-    hobby = 'play computer games',
-    tel = '0638256132'
+    name : 'kathawut',
+    lastname: 'mangkalad',
+    age : 15,
+    nickname :'tam',
+    hobby : 'play computer games',
+    tel : '0638256132'
 }
 console.log(sir)
