8558ea7eb0cf064853600c8264aca4187ce881c0
tamza1094 <50855674+tamza1094@users.noreply.github.com>
2020-09-16 14:38:00 +0700

    Create function.js

---
+++ js/function.js
@@ -0,0 +1,16 @@
+// function add(a,b){
+//     return a+b
+// }
+const add = (a,b) => a+b
+
+console.log("add :",add(2,3))
+
+// function mul(a,b){
+//     return a*b
+// }
+const mul = (a,b) => a*b
+console.log("mul :",mul(2,3))
+
+function searchCustomer(customerName){
+
+}
