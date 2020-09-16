e105b75ccac3a1337bb02ab6cdd65ec83b96c38e
tamza1094 <50855674+tamza1094@users.noreply.github.com>
2020-09-16 15:40:16 +0700

    Create quiz4.js

---
+++ js/quiz4.js
@@ -0,0 +1,3 @@
+const long1 = "Hello my name is Joke".length
+const indexOfJoke = "Hello my name is Joke nice to meet you!".indexOf('Joke')
+console.log("Well open up your mind and see like me".slice(5,9))
