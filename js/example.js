126548b4533db72be088e01fce74eca97a0f7529
tamza1094 <50855674+tamza1094@users.noreply.github.com>
2020-09-16 13:44:06 +0700

    Create example.js

---
+++ js/example.js
@@ -0,0 +1,19 @@
+
+let name = "john"
+name = "JOKE"
+let test ="Hello"
+name = `${test}`
+console.log("name :",name)
+
+let arr = ['apple','banana','kikw']
+console.log(arr[0])
+
+let person ={
+    name : 'joke',
+    age : 15,
+    gender:{
+        name:'male'
+    },
+    arr
+}
+console.log(person)
